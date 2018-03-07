1274aea3c9238d84eaa5c35c9dbd4aad53ea8208
冯强 <fengqiang@kdomi.com>
2018-03-07 13:29:46 +0800

    优化进度显示

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ from progressbar import ProgressBar
 
 def main():
     # 初始化
-    pb = ProgressBar()
+    pb = ProgressBar(' ', '*')
 
     # 更新进度到20%
     pb.progress(percent=0.2)
@@ -18,9 +18,9 @@ def main():
 
     # 一个从1 到 1000进度的显示过程，并伴有详细信息(为模拟进度过程用了0.01的sleep，
     # 实际使用中可以去掉time.sleep)
-    for i in range(1, 1001):
+    for i in range(1000):
         pb.progress(percent=float(i) / 1000, msg='app id -> %d' % i)
-        time.sleep(0.01)
+
     time.sleep(2)
 
 if __name__ == '__main__':
